ebde9db88d735c00659c558713e326a95d4cd5f8
reymartin <reymartinagluya8@gmail.com>
2025-08-13 18:13:16 +0800

    Add

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlayerController;
+
+Route::get('/', [PlayerController::class, 'index'])->name('home');
+
+// Handles form submission and redirects accordingly (player or clan)
+Route::post('/search', [PlayerController::class, 'search'])->name('search');
+    
+// Player profile
+Route::get('/player/{tag}', [PlayerController::class, 'show'])->name('player.show');
+
+// Clan search by name (with ?name=)
+Route::get('/clan', [PlayerController::class, 'searchClans'])->name('clan.search');
+
+// Clan info by tag
+Route::get('/clan/{tag}', [PlayerController::class, 'showClan'])->name('clan.show');
+
+//Ranking
+Route::get('/rankings', [PlayerController::class, 'rankings'])->name('rankings');
